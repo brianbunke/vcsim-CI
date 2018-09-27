c1519d72df1eaecd79eef36ef1ce5aafa48b1e70
Brian Bunke <brianbunke+github@gmail.com>
2018-09-27 13:28:23 -0700

    Install PowerCLI

--- build.ps1
+++ build.ps1
@@ -1,15 +1,10 @@
 # Mac/Linux boxes don't come with Pester
 # Windows has the old v3 in-box version of Pester
-Install-Module Pester -Scope CurrentUser -AllowClobber -SkipPublisherCheck -Force
-
-docker run --rm -d -p 443:443/tcp nimmis/vcsim
-<#
-Install-Module VMware.PowerCLI
+Install-Module Pester, VMware.PowerCLI -Scope CurrentUser -AllowClobber -SkipPublisherCheck -Force
 
 Get-Module Pester, VMware.VimAutomation.Core -ListAvailable
 
-Connect-VIServer -Server 192.168.0.1 -User u -Password p -Port 443
-Get-VM
-#>
+docker run --rm -d -p 443:443/tcp nimmis/vcsim
+
 # Invoke-Pester runs all .Tests.ps1 in the order found by "Get-ChildItem -Recurse"
 Invoke-Pester -OutputFormat NUnitXml -OutputFile ".\TestResults.xml"
